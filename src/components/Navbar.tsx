edbab34ba0ea34fca3efe507fa64e47d0b395e5f
Khoa Le <rubikvn2100@gmail.com>
2022-08-08 03:30:32 -0700

    Refactor navigation bar. Add responsive ability for navigation bar to work with low width device

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,28 +1,56 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import "./Navbar.css";
 
 function Navbar() {
+  const [showMenu, setShowMenu] = useState(false);
   return (
     <nav className="navbar">
-      <Link to="/" className="router-link" id="navbar-name">
-        Khoa Le
-      </Link>
+      <div className="navbar-main-section">
+        <div className="navbar-brand">Khoa Le</div>
+        <span
+          className="material-symbols-outlined toggle-button"
+          onClick={() => setShowMenu(!showMenu)}
+        >
+          menu
+        </span>
+        <div className="navbar-links">
+          <Link to="/" className="router-link navbar-home-link">
+            Home
+          </Link>
 
-      <Link to="/" className="router-link" id="navbar-home-link">
-        Home
-      </Link>
+          <Link to="/about" className="router-link">
+            About me
+          </Link>
 
-      <Link to="/about" className="router-link">
-        About me
-      </Link>
-
-      <Link to="/portfolios" className="router-link">
-        Portfolios
-      </Link>
-      <div>
-        <button id="navbar-contact-button">Contact</button>
+          <Link to="/portfolios" className="router-link">
+            Portfolios
+          </Link>
+          <div>
+            <button className="navbar-contact-button">Contact</button>
+          </div>
+        </div>
       </div>
+      {showMenu && (
+        <div className="navbar-dropdown-menu">
+          <div className="navbar-links">
+            <Link to="/" className="router-link navbar-home-link">
+              Home
+            </Link>
+
+            <Link to="/about" className="router-link">
+              About me
+            </Link>
+
+            <Link to="/portfolios" className="router-link">
+              Portfolios
+            </Link>
+            <div>
+              <button className="navbar-contact-button">Contact</button>
+            </div>
+          </div>
+        </div>
+      )}
     </nav>
   );
 }
